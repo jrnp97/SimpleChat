2581619f4384e98d1b197ca94fa9f321e4ba94cc
jrnp97 <jrnp1997@gmail.com>
2021-03-30 00:46:43 -0300

    :construction: Creating first model of profile page and include chat app bases

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,14 +2,23 @@
 
 from django.urls import path
 
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 
 from accounts.views import RegistrationView
 
-
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_uri'),
     path('logout/', LogoutView.as_view(), name='logout_uri'),
     path('register/', RegistrationView.as_view(), name='registration_uri'),
+    path(
+        'profile/',
+        login_required(
+            TemplateView.as_view(template_name='accounts/profile.html')
+        ),
+        name='profile_uri',
+    ),
 ]
